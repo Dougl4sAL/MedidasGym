a687a7570a140eb72a48e92d2b2794e4b403b29c
Douglas Almeida <dougsouza1708@gmail.com>
2025-04-14 23:44:13 -0300

    Organização da estrutura depois do React

---
+++ medidas-gym/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Medidas GYM</h1>
+      <p>Seu projeto React funcionadno</p>
+    </div>
+  );
+}
+
+export default App;
