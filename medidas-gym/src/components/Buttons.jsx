13c86d6381bd08219d7d3027679a2df6e56df835
Douglas Almeida <dougsouza1708@gmail.com>
2025-04-16 21:24:05 -0300

    Inicio da implemetação do react

---
+++ medidas-gym/src/components/Buttons.jsx
@@ -0,0 +1,14 @@
+export default function VoltarHome() {
+    //Botão para voltar pra Home
+
+}
+
+export default function AdicionarMedida() {
+    //Botão para ir na pagina Adicionar Medida
+
+}
+
+export default function RelatorioCorporal() {
+    //Botão para voltar pra relatorio corpotal
+
+}
